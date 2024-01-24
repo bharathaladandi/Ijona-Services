810355db452bb87b1d343b3a319170a39050f9a0
bharathaladandi <bharathaladandi@gmail.com>
2024-01-25 01:30:35 +0530

    delete function added

--- ijona/src/Pages/HomaPage.jsx
+++ ijona/src/Pages/HomaPage.jsx
@@ -1,20 +1,28 @@
-import React, {useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Auth } from '../Context/AuthProvider';
 
 
 export const HomaPage = () => {
 
-  const { user } = Auth();
+  const { user, logout } = Auth();
 
   const [data, setData] = useState([
     { id: 1, name: 'user 1', description: 'Description 1' },
     { id: 2, name: 'user 2', description: 'Description 2' },
+    { id: 3, name: 'user 3', description: 'Description 3' },
+    { id: 4, name: 'user 4', description: 'Description 4' },
+    { id: 5, name: 'user 5', description: 'Description 5' },
+    { id: 6, name: 'user 6', description: 'Description 6' },
   ]);
 
+  const handleDelete = (id) => {
+    setData((prevData) => prevData.filter((item) => item.id !== id));
+  };
   return (
     <div>
       <h2>Home Page</h2>
       <p>Welcome, {user && user.username}!</p>
+      <button onClick={logout}>Logout</button>
       <table>
         <thead>
           <tr>
@@ -31,6 +39,7 @@ export const HomaPage = () => {
               <td>{item.name}</td>
               <td>{item.description}</td>
               <td>
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
               </td>
             </tr>
           ))}
