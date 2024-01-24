557a693a9fc77d2ca63e9dcc28b4952ee5463a5d
bharathaladandi <bharathaladandi@gmail.com>
2024-01-24 22:37:23 +0530

    login page created

--- ijona/src/Pages/HomaPage.jsx
+++ ijona/src/Pages/HomaPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export const HomaPage = () => {
+
   return (
-    <div>HomaPage</div>
+    <div>HomePage</div>
   )
 }
--- ijona/src/Pages/Ragister.jsx
+++ ijona/src/Pages/Ragister.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Ragister = () => {
-  return (
-    <div>Ragister</div>
-  )
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div>
+            <h2>Login Page</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h3>Username :
+                        <input type="text" value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
+                    </h3>
+                </div>
+                <br />
+
+                <div>
+                    <h3>Password :
+                        <input type="password" value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </h3>
+                </div>
+                <br />
+                <button type='submit'> Login </button>
+            </form>
+        </div>
+    )
 }
