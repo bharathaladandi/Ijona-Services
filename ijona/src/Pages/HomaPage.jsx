a6ee484dd7fc418a322fd218eede1c438294f095
bharathaladandi <bharathaladandi@gmail.com>
2024-01-25 11:07:47 +0530

    data added

--- ijona/src/Pages/HomaPage.jsx
+++ ijona/src/Pages/HomaPage.jsx
@@ -27,8 +27,11 @@ export const HomaPage = () => {
   const { user, logout, isAuthenticated } = Auth();
 
   const [data, setData] = useState([
-    { id: 1, name: 'Item 1', description: 'Description 1' },
-    { id: 2, name: 'Item 2', description: 'Description 2' },
+    { id: 1, user: 'User 1', description: 'Description 1' },
+    { id: 2, user: 'User 2', description: 'Description 2' },
+    { id: 3, user: 'User 3', description: 'Description 3' },
+    { id: 4, user: 'User 4', description: 'Description 4' },
+    { id: 5, user: 'User 5', description: 'Description 5' },
   ]);
 
   const [dataItems, setDataitems] = useState({ name: '', description: '' });
